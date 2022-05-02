d669d328656c718b720f3f01200bfe3763d0c2b3
RasikaLakmal <drasikalakmal@gmail.com>
2022-05-02 16:45:58 +0530

    update02052022_1

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,10 @@ import { createConnection } from 'typeorm';
 import * as BodyParser from 'body-parser';
 import cors from 'cors';
 import postRoutes from './routes/postRoutes';
-import {User } from '../src/entity/User'
-import {Issues } from '../src/entity/Issues'
-import {Counter } from '../src/entity/Counter'
-import {Counter_person } from '../src/entity/Counter_person'
-import {Notifications } from '../src/entity/Notifications'
 const app = express();
 
 app.use(express.json());
 //const port:number = 3000;
-createConnection({
-    type: 'mysql',
-    database: "queue",
-    username: "root",
-    password: "",
-    logging: true,
-    synchronize: true,
-    entities: [User,Issues,Counter,Counter_person,Notifications]
-
-})
 
 createConnection().then(async (connection) => {
     const app = express();
@@ -45,16 +30,3 @@ app.route("/")
 .post((req:Request,res:Response)=>{
    return res.send('post req');
 });
-app.post('/api/user',(req:Request,res:Response)=>{
-    User.insert({
-        u_email : "5",
-        password : "abc",
-        name : "lakmal",
-        phone_no : 234566,
-        
-        
-        
-    });
-
-    res.end();
-})
